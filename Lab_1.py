1c4b24a822c727bc248c07fa5b7d4b1a223afd9a
Christian <126699654+morghie@users.noreply.github.com>
2023-03-01 17:14:43 +0000

    zadania 1, 2, 3

--- Lab_1.py
+++ Lab_1.py
@@ -7,11 +7,11 @@
 # a = 2  # zmienna a ma typ integer (liczba całkowita)
 # b = 7.345134   # zmienna b typ float (liczba zmiennoprzecinkowa)
 # c = a + b
-# # d = b ** a  # ** to operator potęgowania  b^a
-# # print(a)
-# # print(b)
+# d = b ** a  # ** to operator potęgowania  b^a
+# print(a)
+# print(b)
 # print(c)
-# # print(d)
+# print(d)
 # print(type(b)) # fun type sprawdza typ zmiennej
 # print("#####################")
 # z = 'Ala'
@@ -19,7 +19,7 @@
 # y = "koty"
 # zdanie = y + x + z
 # print(zdanie)
-#print(y,x,z)
+# print(y,x,z)
 # X = 2
 # Y = '2'
 # print("Zmienna X ma typ: ",type(X))
@@ -57,16 +57,44 @@
 # print(a >= b)
 
 ##############Zadania do wykonania, Twoje pierwsze algorytmy
+
 # 1. Wykonaj odejmowanie, mnożenie i dzielenie 2 dowolnych liczb
+
+# x = float(input('Podaj x = '))
+# y = float(input('Podaj y = '))
+# suma = x + y
+# print('Suma to: ', suma)
+
+
 # 2. Oblicz wyrażenie 2x+5y   gdzie: x,y to dowolne dwie liczby które podaje użytkownik (w konsoli)
+
+# print('Zadanie #2.')
+# x = float(input('Ustaw x: '))
+# y = float(input('Ustaw y: '))
+# wyr = 2*x + 5*y
+# print('Wyrażenie to: ', wyr)
+
+
 # 3. Popraw zmienną zdanie tak aby wyświetlany był napis: "Ala ma kota"
+
+zdanie = 'Ala ma kota'
+print(zdanie)
+
+
 # 4. Wyświetl zdanie "Jestem a b mam c lat studiuję d",
+
+a = str
 #  gdzie : a-imie, a-nazwisko, c-liczba, d-kierunek studiów są dowolne zmienne które podaje użytkownik (wczytywane z klawiatury)
 # 5. Sprawdź czy 1+2+10+20000001+4+347586970885 jest równa 321784560456434534646
 # 6. Sprawdź czy suma dowolnych dwóch liczb podanych przez użytkownika jest liczbą parzystą czy nieparzystą wyświetl właściwy komunikat
 # 7. Utwórz prosty kalkulator dla 2 zmiennych podanych przez użytkownika, który obliczy: sumę, różnicę,
 # iloczyn, iloraz, potęgę tych liczb, nie zapomnij o stosownych komentarzach informacyjnych dla użytkownika.
 # 8. Oblicz wyrażenie: a = 3z-|2cos(x)sin(y)|, gdzie: x,y,z - dowolne liczby     (|x| to moduł z liczby z, użyj funkcji abs())
+
+# from math import sin,cos
+# a =sin(30)
+# print(a)
+# print(abs(a))
 
 # 9. Wykonaj mini ankietę tj. poproś użytkownika o następujące informacje: imie, nazwisko, wiek, zadaj mu pytania: "Czy zdrowo się odżywiasz?",
 # , "Czy lubisz sport?" i dodatkowo 3 inne własne. Po uzyskaniu wszystkich odpowiedzi wyświetl ich podsumowanie.
