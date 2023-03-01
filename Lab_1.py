ff9541b6b6791535e74d5c1733a5b59faa268e87
Christian <>
2023-03-01 16:16:14 +0000

    Initial commit

---
+++ Lab_1.py
@@ -0,0 +1,88 @@
+## Zapoznaj się z prostymi typami zmiennych
+# print('Hello world') # fun print wyświetla wynik
+# name_of_car = 'mercedes'  # zmienna name_of_car jest typu string
+# print(name_of_car)
+# print('Lubię samochód marki mercedes a moja rodzina też lubi samochód marki: mercedes')
+############Przyklad 1
+# a = 2  # zmienna a ma typ integer (liczba całkowita)
+# b = 7.345134   # zmienna b typ float (liczba zmiennoprzecinkowa)
+# c = a + b
+# # d = b ** a  # ** to operator potęgowania  b^a
+# # print(a)
+# # print(b)
+# print(c)
+# # print(d)
+# print(type(b)) # fun type sprawdza typ zmiennej
+# print("#####################")
+# z = 'Ala'
+# x = "ma"
+# y = "koty"
+# zdanie = y + x + z
+# print(zdanie)
+#print(y,x,z)
+# X = 2
+# Y = '2'
+# print("Zmienna X ma typ: ",type(X))
+# print("Zmienna Y ma typ: ",type(Y))
+# print(z,"ma kotów: ",str(X))
+# print(z,"ma kotów: ",Y)
+# print(X+Y)
+# print("#####################")
+# zm1 = input("Podaj zmienną x =") # fun input służy do wprowadzania wartości przez uzytkownika w konsoli
+# zm2 = input("Podaj zmienną y =") # wynik funkcji (argument wyjściowy funkcji) input ma typ string
+# c = zm1 + zm2  # konwersję zmiennej string na  float
+# print(c)
+
+# zm1 = input("Podaj zmienną x =") # fun imput służy do wprowadzania wartości przez uzytkownika w konsoli
+# zm2 = input("Podaj zmienną y =") # wynik funkcji (argument wyjściowy funkcji) input ma typ string
+# c = float(zm1) + float(zm2)  # konwersję zmiennej string na  float
+# print(c)
+#print("#####################")
+
+# usun = input("Podaj zmienną ktorą chcesz usunąć z pamięci: ")
+# del(usun)
+
+#print("#####################")
+############Zmienne logiczne ###############
+# a = True
+# b = False # zmienna b typ logiczny bool
+# print(type(a))
+# x = '1'
+# y = 1
+# print(x == y)   # operator ==  oznacza czy x równe y?
+# print(x != y)   # operator !=  oznacza czy x różny od y?
+# a = 3
+# b = 5
+# print(a < b)
+# print(a >= b)
+
+##############Zadania do wykonania, Twoje pierwsze algorytmy
+# 1. Wykonaj odejmowanie, mnożenie i dzielenie 2 dowolnych liczb
+# 2. Oblicz wyrażenie 2x+5y   gdzie: x,y to dowolne dwie liczby które podaje użytkownik (w konsoli)
+# 3. Popraw zmienną zdanie tak aby wyświetlany był napis: "Ala ma kota"
+# 4. Wyświetl zdanie "Jestem a b mam c lat studiuję d",
+#  gdzie : a-imie, a-nazwisko, c-liczba, d-kierunek studiów są dowolne zmienne które podaje użytkownik (wczytywane z klawiatury)
+# 5. Sprawdź czy 1+2+10+20000001+4+347586970885 jest równa 321784560456434534646
+# 6. Sprawdź czy suma dowolnych dwóch liczb podanych przez użytkownika jest liczbą parzystą czy nieparzystą wyświetl właściwy komunikat
+# 7. Utwórz prosty kalkulator dla 2 zmiennych podanych przez użytkownika, który obliczy: sumę, różnicę,
+# iloczyn, iloraz, potęgę tych liczb, nie zapomnij o stosownych komentarzach informacyjnych dla użytkownika.
+# 8. Oblicz wyrażenie: a = 3z-|2cos(x)sin(y)|, gdzie: x,y,z - dowolne liczby     (|x| to moduł z liczby z, użyj funkcji abs())
+
+# 9. Wykonaj mini ankietę tj. poproś użytkownika o następujące informacje: imie, nazwisko, wiek, zadaj mu pytania: "Czy zdrowo się odżywiasz?",
+# , "Czy lubisz sport?" i dodatkowo 3 inne własne. Po uzyskaniu wszystkich odpowiedzi wyświetl ich podsumowanie.
+# 10. Twoim zadaniem jest przygotowanie uniwersalnego i profesjonalnego życiorysu, złożonego z 10-ciu zdań, które wyświetlisz na ekranie
+# Użytkownik wpisuje tylko swoje imie, nazwisko, wiek, zawód, miejsce urodzenia, zainteresowania i ... życiorys jest gotowy.
+# 11. Przygotuj dla dziecka, które uczy się czytać zestaw sylab do nauki, ale zrób to inteligentnie tj.
+# dziecko wpisuje na klawiaturze 1 spółgłoskę a Ty dodajesz do niej odpowiednie samogłoski i wyświetlasz całość na ekranie
+#12. Sprawdz wynik działań
+# 0 > 1
+# 0 <= 1
+# 0 >= 1
+# 1 == 0
+# 1 == 1
+# 1 != 0
+# 1 != 1
+#(x > 1 and x < 13) and x != 5  , dla x = 2
+# 13. Użytkownik podaje imie, sprawdź czy to imie to Janusz lub Grażyna
+#
+#print("#####################")
---
+++ main.py
@@ -0,0 +1,1 @@
+import Lab_1
